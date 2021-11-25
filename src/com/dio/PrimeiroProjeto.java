65038c1d2ab0f8f3dd114343a6f5916ed4480945
Fernando <lfernando.viola@gmail.com>
2021-11-25 13:24:56 -0300

    First Commit

--- src/com/dio/PrimeiroProjeto.java
+++ src/com/dio/PrimeiroProjeto.java
@@ -5,16 +5,16 @@ import com.dio.model.Gato;
 public class PrimeiroProjeto {
 
 	public static void main(String[] args) {
-		
+
 		Gato gato = new Gato();
 		Livros livros = new Livros();
-		
-	System.out.println (gato);
-    System.out.println(livros);
-		
-		/*int a = 13;
-		int b = 8;
-       System.out.println("Hello Fucking World!" +   (a+b));*/
+
+		System.out.println(gato);
+		System.out.println(livros);
+
+		/*
+		 * int a = 13; int b = 8; System.out.println("Hello Fucking World!" + (a+b));
+		 */
 	}
 
 }
